636c76cc6978668eee88cd352cc9ab680f112eef
emamakhan <emamak000@gmail.com>
2021-11-04 19:49:50 +0500

    login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dads discount
-        </a>
-      </header>
-    </div>
+      <div className="card col-12 col-lg-4 login-card mt-2 hv-center abc">
+          <form>
+              <div className="form-group text-left">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" 
+                     className="form-control" 
+                     id="email" 
+                     aria-describedby="emailHelp" 
+                     placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group text-left">
+                  <label htmlFor="exampleInputPassword1">Name</label>
+                  <input type="text" 
+                      className="form-control" 
+                      id="text" 
+                      placeholder="Name"
+                  />
+              </div>
+              <div className="form-group text-left">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input type="password" 
+                      className="form-control" 
+                      id="confirmPassword" 
+                      placeholder="Password"
+                  />
+              </div>
+              <button 
+                  type="submit" 
+                  className="btn btn-primary"
+              >
+                  Register
+              </button>
+          </form>
+      </div>
   );
 }
 
